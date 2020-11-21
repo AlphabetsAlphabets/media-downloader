dece9344d8253b48585d8aaede4dd377a0188277
Yap Jia Hong <yapjiahong2003@gmail.com>
2020-11-21 22:32:14 +0800

    Added more changes to code to increase download speed. Number of total images increased.

--- src/image.py
+++ src/image.py
@@ -1,14 +1,14 @@
-import requests, json, aiohttp, requests, asyncio
+import requests, json, aiohttp, requests, asyncio, random, time
 import os, sys
 
 from base import *
 
 class Unsplash(Link):
     creds = {
-    "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Encoding" : "gzip, deflate, br",
-    "Accept-Language" : "en-US,en;q=0.5",
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0"
+        "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding" : "gzip, deflate, br",
+        "Accept-Language" : "en-US,en;q=0.5",
+        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0"
     }
 
     resos = ['raw', 'full', 'regular', 'small', 'thumb']
@@ -20,39 +20,47 @@ class Unsplash(Link):
         else:
             self.quality = "small"
 
-        self.url = f"https://unsplash.com/napi/search?query={self.term}&xp=&per_page=20"
+        self.url = f"https://unsplash.com/napi/search?query={self.term}&xp=&per_page=50"
 
     def harvest_link(self):
         r = requests.get(self.url, headers=self.creds).json()
         total = r['photos']['results']
-        images = [x['urls'][self.quality] for x in total]
+        self.totalImages = len(total)
+        self.link = [x['urls'][self.quality] for x in total]
 
-        return images
+        return self.link
 
-    async def download(self):
-        images = self.harvest_link()
+    async def queue(self, link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as r:
+                bytes = await r.read()
+                count = random.randint(1, 1000) / 939
+                with open(f"{self.path}\\{self.term}_{count}.png", 'wb') as f:
+                    f.write(bytes)
 
+    async def path_creation(self):
         exists = os.path.exists("content")
         if not exists:
             os.mkdir("content")
-            path = os.getcwd() + "\\content"
+            self.path = os.getcwd() + "\\content"
         else:
-            path = os.getcwd() + "\\content"
+            self.path = os.getcwd() + "\\content"
 
-        async with aiohttp.ClientSession() as session:
-            for count, links in enumerate(images, start=1):
-                async with session.get(links) as r:
-                    bytes = await r.read()
-                    with open(f"{path}\\image{count}.png", 'wb') as f:
-                        f.write(bytes)
-
-        sys.exit()
+        links = [self.queue(link) for link in self.harvest_link()]
+        await asyncio.wait(links)
 
 def main():
     term = input("Images of what you want to download: ")
+    # term = "cats"
     un = Unsplash(term)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(un.download())
+
+    currentTime = time.time()
+    loop.run_until_complete(un.path_creation())
+    finishTime = time.time() - currentTime
+
+    print(F"Task completed in: {finishTime} seconds")
+    print(f"Downloaded a total of: {un.totalImages} images")
 
 if __name__ == "__main__":
     main()
